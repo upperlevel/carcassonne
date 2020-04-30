71cb584a92e2da907ebab6a46f8425239ed358b0
Rossi Lorenzo <snowycoder@gmail.com>
2020-04-30 12:49:01 +0200

    Implement isCompatible

--- src/game/cardTile.ts
+++ src/game/cardTile.ts
@@ -8,8 +8,16 @@ export class CardTile {
         this.card = card;
     }
 
+    getSide(side: Side): SideType {
+        switch (side % Side.size) {
+            case Side.LEFT: return this.card.sides.left;
+            case Side.RIGHT: return this.card.sides.right;
+            case Side.BOTTOM: return this.card.sides.bottom;
+            case Side.TOP: return this.card.sides.top;
+        }
+    }
+
     isCompatible(side: Side, placedCard: CardTile): boolean {
-        // TODO
-        return false;
+        return this.getSide(side) == placedCard.getSide(side + 2)
     }
 }
