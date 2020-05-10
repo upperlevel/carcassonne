fe9f82cb7d6a8b967fb6557eb0d04ccbda393398
loryruta <loryruta23@gmail.com>
2020-05-10 11:11:06 +0200

    Add hint-bar

--- src/ui/game/game.vue
+++ src/ui/game/game.vue
@@ -1,3 +1,5 @@
+import {RoundState} from "../../phase/gamePhase";
+import {RoundState} from "../../phase/gamePhase";
 <template>
     <div class="game">
         <!-- PlayerBar -->
@@ -7,6 +9,10 @@
                 :players="this.players"
         >
         </player-bar-component>
+
+        <!-- ActionBar -->
+        <div class="hint-bar" v-html="getRoundStateHint()">
+        </div>
 
         <!-- GameBar -->
         <game-bar-component
@@ -25,6 +31,7 @@
 
     import PlayerBarComponent from "./playerBar.vue";
     import GameBarComponent from "./gameBar.vue";
+    import {RoundState} from "../../phase/gamePhase";
 
     export default Vue.extend({
         /* Passed on initialization.
@@ -40,6 +47,35 @@
             PlayerBarComponent,
             GameBarComponent
         },
+        methods: {
+            colorToStr(color: number) {
+                return `
+                    rgb(
+                        ${(color >> 16) & 0xff},
+                        ${(color >> 8) & 0xff},
+                        ${color & 0xff}
+                    )
+                `;
+            },
+
+            getRoundStateHint() {
+                if (this.gamePhase.isMyRound()) {
+                    switch (this.gamePhase.roundState) {
+                        case RoundState.CardDraw:
+                            return `<h4 style="color: yellow">Yo! It's your turn, <u>draw a card</u>.</h4>`;
+                        case RoundState.CardPlace:
+                            return `<h4 style="color: yellow"><u>Place the card</u> on the board.</h4>`;
+                        case RoundState.PawnPick:
+                            return `<h4 style="color: yellow"><u>Skip</u> the round or <u>pick a pawn</u>.</h4>`;
+                        case RoundState.PawnPlace:
+                            return `<h4 style="color: yellow"><u>Place the pawn</u> within the card you've placed.</h4>`;
+                    }
+                }
+                const roundOf = this.gamePhase.roundOf;
+                const roundOfColor = this.colorToStr(roundOf.color);
+                return `<h4 style="color: red">It's the turn of: <span style="color: ${roundOfColor}">${roundOf.username}</span>.</h4>`;
+            }
+        }
     });
 </script>
 
@@ -48,11 +84,25 @@
         user-select: none;
     }
 
+    .hint-bar {
+        position: absolute;
+
+        bottom: 80px;
+        width: 100%;
+
+        z-index: 10;
+
+        pointer-events: none;
+
+        text-align: center;
+        text-shadow: -1px -1px 0 black, 1px -1px 0 black, -1px  1px 0 black, 1px  1px 0 black;
+    }
+
     .game-bar {
         position: absolute;
         bottom: 0;
         width: 100%;
-        z-index: 1;
+        z-index: 25;
     }
 
     .player-bar {
